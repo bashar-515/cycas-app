74bc56708355c3644c0f544f65f64af08108c9b7
Bashar Eid <bashar.eid@viam.com>
2025-09-19 12:01:52 -0400

    Initial Commit

---
+++ Cycas/CycasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycasApp.swift
+//  Cycas
+//
+//  Created by Bashar Eid on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct CycasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CycasTests/CycasTests.swift
@@ -0,0 +1,17 @@
+//
+//  CycasTests.swift
+//  CycasTests
+//
+//  Created by Bashar Eid on 9/19/25.
+//
+
+import Testing
+@testable import Cycas
+
+struct CycasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
