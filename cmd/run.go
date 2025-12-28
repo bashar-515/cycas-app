abe13cdbf8f6622c0d4ee809e213dee2798bad56
Bashar Eid <beesho.eid@gmail.com>
2025-12-27 23:08:22 -0600

    checkpoint

--- cmd/run.go
+++ cmd/run.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"codeberg.org/cycas/app/src/lib/server"
 )
 
 func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
 	handler, err := server.NewServer().Handler()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("error getting server handler: %v", err)
 	}
 
-	http.ListenAndServe(":8000", handler)
+	srv := &http.Server{
+		Addr: ":8000",
+		Handler: handler,
+		// TODO: [q] do any other fields need to be set?
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("error listening and serving: %v", err)
+		}
+	}()
+	
+	<-ctx.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err = srv.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("error shutting server down: %v", err)
+	}
 }
--- src/lib/server/server.go
+++ src/lib/server/server.go
@@ -1,1 +1,44 @@
 package server
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/oapi-codegen/nethttp-middleware"
+
+	"codeberg.org/cycas/app/gen/api"
+)
+
+type Server struct{}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) Handler() (http.Handler, error) {
+	swagger, err := api.GetSwagger()
+	if err != nil {
+		return nil, err
+	}
+
+	handler := api.Handler(api.NewStrictHandler(s, []api.StrictMiddlewareFunc{}))
+	options := nethttpmiddleware.Options{SilenceServersWarning: true }
+
+	return nethttpmiddleware.OapiRequestValidatorWithOptions(swagger, &options)(handler), nil
+}
+
+func (s *Server) CreateCategory(
+	ctx context.Context,
+	request api.CreateCategoryRequestObject,
+) (api.CreateCategoryResponseObject, error) {
+	if request.Body == nil {
+		return nil, errors.New("request body is nil")
+	}
+
+	if request.Body.Name == "foo" {
+		return api.CreateCategory201JSONResponse{Id: "abc123", Name: request.Body.Name}, nil
+	}
+
+	return api.CreateCategory409JSONResponse{Error: "Category must be named foo"}, nil
+}
