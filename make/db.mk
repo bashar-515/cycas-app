bac4db5e5ce87d7b3436a4724631dc0982906f14
Bashar Eid <beesho.eid@gmail.com>
2026-01-02 16:21:24 -0600

    set up database

--- make/db.mk
+++ make/db.mk
@@ -1,5 +1,21 @@
-.PHONY: db-up db-down
+.PHONY: db-up db-down db-clean
+
+CONTAINER_NAME := cycas-db
 
 db-up:
+	@if podman container exists $(CONTAINER_NAME); then \
+		podman start $(CONTAINER_NAME); \
+	else \
+		podman run \
+			--name  $(CONTAINER_NAME) \
+			--env POSTGRES_PASSWORD=mysecretpassword \
+			--publish 5433:5432 \
+			--detach \
+			postgres; \
+	fi
 
 db-down:
+	podman stop $(CONTAINER_NAME)
+
+db-clean:
+	podman rm $(CONTAINER_NAME)
