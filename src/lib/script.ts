6c7d15b1e44018d76b93c39e9059b1d60008f4cc
Bashar Eid <beesho.eid@gmail.com>
2026-01-02 03:41:38 -0600

    nullify script.ts

--- src/lib/script.ts
+++ src/lib/script.ts
@@ -1,10 +1,10 @@
-import * as Cycas from '@beeesho/cycas';
-import { PUBLIC_CYCAS_API_URI } from '$env/static/public';
-
-const configuration = Cycas.createConfiguration({
-	baseServer: new Cycas.ServerConfiguration(PUBLIC_CYCAS_API_URI, {})
-});
-
-const api = new Cycas.DefaultApi(configuration);
-
-console.log(api);
+// import * as Cycas from '@beeesho/cycas';
+// import { PUBLIC_CYCAS_API_URI } from '$env/static/public';
+//
+// const configuration = Cycas.createConfiguration({
+// 	baseServer: new Cycas.ServerConfiguration(PUBLIC_CYCAS_API_URI, {})
+// });
+//
+// const api = new Cycas.DefaultApi(configuration);
+//
+// console.log(api);
