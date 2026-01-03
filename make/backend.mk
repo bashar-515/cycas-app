715fe8f50cd14daa7f6006c2efe51a77fd17d5ae
Bashar Eid <beesho.eid@gmail.com>
2026-01-02 23:54:45 -0600

    checkpoint

--- make/backend.mk
+++ make/backend.mk
@@ -3,16 +3,16 @@
 backend-up: setup-backend
 	go tool air
 
-setup-backend: gen tidy
+setup-backend: gen-app tidy
 
-.PHONY: gen gen-server gen-models setup-gen
+.PHONY: gen-app gen-server gen-models setup-gen
 
 GEN := go tool oapi-codegen
 
 SPEC_FILE := api/spec/openapi.yaml
 CFG_DIR := api/config/server
 
-gen: gen-models gen-server gen-spec tidy
+gen-app: gen-models gen-server gen-spec tidy
 
 gen-models: setup-gen
 	$(GEN) -config $(CFG_DIR)/models.yaml $(SPEC_FILE)
--- make/web.mk
+++ make/web.mk
@@ -3,5 +3,17 @@
 web-up: setup-web
 	npm run dev
 
-setup-web:
+setup-web: install
+
+.PHONY: gen-sdk
+
+gen-sdk: install
+	openapi-generator generate \
+		--generator-name typescript \
+		--output gen/sdk \
+		--input-spec api/spec/openapi.yaml \
+
+.PHONY: install
+
+install:
 	pnpm install
