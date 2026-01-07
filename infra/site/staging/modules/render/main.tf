3f282f882954a521bb80c908409c279bed3c45cd
Bashar Eid <beesho.eid@gmail.com>
2026-01-07 00:19:21 -0500

    change site repository

--- infra/site/staging/modules/render/main.tf
+++ infra/site/staging/modules/render/main.tf
@@ -2,13 +2,13 @@ resource "render_static_site" "main" {
     branch = "main"
     build_command = ""
     name = "cycas site"
-    repo_url = "https://github.com/bashar-515/cycas-site"
+    repo_url = "https://github.com/bashar-515/cycas-app"
 
     auto_deploy = true
 
     build_filter = {
       paths = [
-        "public/**",
+        "site/public/**",
       ]
     }
 
