c42af0945414ab475eace0e791c662616e207993
Bashar Eid <beesho.eid@gmail.com>
2026-01-13 00:09:38 -0500

    checkpoint

---
+++ infra/app/local/terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    zitadel = {
+      source = "zitadel/zitadel"
+      version = "2.6.0"
+    }
+  }
+}
+
+provider "zitadel" {
+  domain           = "localhost"
+  insecure         = "true"
+  port             = "8080"
+  jwt_profile_file = "local-token"
+}
